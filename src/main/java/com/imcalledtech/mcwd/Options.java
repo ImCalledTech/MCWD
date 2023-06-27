85b470e66ad5fceb1609c9ed1cb45bf91153e2e9
TechWasTaken <lewisbrame@outlook.com>
2023-06-27 18:59:04 +0100

    creation of options class and add gson to pom

---
+++ src/main/java/com/imcalledtech/mcwd/Options.java
@@ -0,0 +1,7 @@
+package com.imcalledtech.mcwd;
+
+import com.google.gson.*;
+
+public class Options {
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.imcalledtech.mcwd {
     requires javafx.controls;
     requires javafx.fxml;
     requires commons.io;
+    requires com.google.gson;
 
 
     opens com.imcalledtech.mcwd to javafx.fxml;
